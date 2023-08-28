98656ed86484e4b59b72b55c9b07a3b694d544fd
wlp26 <c14200150@john.petra.ac.id>
2023-08-28 19:05:01 +0700

    update login

--- driver_cold_storage/lib/screens/login.dart
+++ driver_cold_storage/lib/screens/login.dart
@@ -10,54 +10,94 @@ class _loginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            Container(
-              padding:
-                  EdgeInsets.only(left: 56, right: 206, top: 189, bottom: 58),
-              child: Text(
-                "Sign In",
-                style: TextStyle(
-                  fontFamily: 'Sora',
-                  color: Color(0xFF6AD6F9),
-                  fontSize: 36,
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.w700,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 32.0, top: 200),
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                      color: Color(0xFF6AD6F9),
+                      fontSize: 36,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Sora"),
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Column(children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 16),
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "Username"),
-                  ),
+              Padding(
+                padding: EdgeInsets.only(left: 32, right: 32, top: 24),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Username",
+                      hintStyle: TextStyle(
+                          fontSize: 20,
+                          color: Colors.grey,
+                          fontFamily: "Sora")),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 33.5),
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "Password"),
-                  ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 32, right: 32, top: 24),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(
+                          fontSize: 20,
+                          color: Colors.grey,
+                          fontFamily: "Sora")),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 85, left: 16, right: 157),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 40, right: 32, top: 16),
+                child: InkWell(
+                  onTap: () {
+                    print('Tombol teks ditekan!');
+                  },
                   child: Text(
-                    "Forgot Password",
+                    'Forget Password',
                     style: TextStyle(
-                        fontFamily: 'Sora',
-                        fontSize: 16,
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xFF6AD6F9)),
-                    textAlign: TextAlign.center,
+                        color: Color(0xFF6AD6F9),
+                        decoration: TextDecoration.underline,
+                        fontSize: 18,
+                        fontFamily: "Sora"),
                   ),
                 ),
-                ElevatedButton(onPressed: () {}, child: Text('Login'))
-              ]),
-            )
-          ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 100), // Padding horizontal 32 dan vertikal 24
+                child: Container(
+                  width: double
+                      .infinity, // Ini akan membuat Container mengambil lebar maksimal yang tersedia.
+                  height:
+                      50, // Anda bisa mengatur tinggi tombol sesuai kebutuhan Anda.
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              30), // border radius sebesar 30
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(5),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF6AD6F9)),
+                    ),
+                    onPressed: () {
+                      print("Button Login ditekan");
+                    },
+                    child: Text('Login',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Sora",
+                        )), // Anda juga bisa mengatur style dari teks jika diperlukan.
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
