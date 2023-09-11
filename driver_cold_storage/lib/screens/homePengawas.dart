ff0631bb4bd7b927ff18734f1f5d7831e7df8635
wlp26 <c14200150@john.petra.ac.id>
2023-09-11 22:01:06 +0700

    Update homePengawas.dart

--- driver_cold_storage/lib/screens/homePengawas.dart
+++ driver_cold_storage/lib/screens/homePengawas.dart
@@ -14,6 +14,7 @@ class _homePengawasState extends State<HomePengawas> {
         SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
                 padding: EdgeInsets.only(
@@ -34,6 +35,147 @@ class _homePengawasState extends State<HomePengawas> {
                 child: Divider(
                   height: 1, // Atur tinggi garis sesuai kebutuhan
                   color: Colors.grey, // Atur warna garis sesuai kebutuhan
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 32,
+                  top: 16,
+                ),
+                child: Text(
+                  "Last Input",
+                  style: TextStyle(
+                      color: Color(0xFF6AD6F9),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Sora"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, top: 8),
+                child: Container(
+                  width: 360,
+                  height: 152,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Left Side Content
+                      Flexible(
+                        // <-- Tambahkan ini
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // ID Section
+                            Row(
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 8.0, left: 8),
+                                  child: Text(
+                                    "ID:",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily: "Sora",
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                      top: 8,
+                                      left: 8,
+                                    ),
+                                    child: Text(
+                                      "BJE454879BJEP",
+                                      style: TextStyle(
+                                        color: Color(0xFF6AD6F9),
+                                        fontSize: 22,
+                                        fontFamily: "Sora",
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8, top: 4),
+                              child: Text(
+                                "7 Juli 2023 | 10:00",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontFamily: "Sora",
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8, top: 4),
+                              child: Text(
+                                "Daging ayam",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontFamily: "Sora",
+                                ),
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 8, top: 8),
+                                  child: Text(
+                                    "Note :",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 13,
+                                      fontFamily: "Sora",
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding:
+                                        const EdgeInsets.only(left: 8, top: 8),
+                                    child: Text(
+                                      "jhjsahd jsahdjas sad asdsa hjdhzf sdfsafsdfs",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 13,
+                                        fontFamily: "Sora",
+                                      ),
+                                      maxLines: 2,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8, right: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ..._buildDetailColumn("Items", "20"),
+                            SizedBox(height: 8),
+                            ..._buildDetailColumn("Weight", "20kg"),
+                            SizedBox(height: 8),
+                            ..._buildDetailColumn("Stops", "2"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -81,4 +223,25 @@ class _homePengawasState extends State<HomePengawas> {
       ])),
     );
   }
+}
+
+List<Widget> _buildDetailColumn(String title, String value) {
+  return [
+    Text(
+      title,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontFamily: "Sora",
+      ),
+    ),
+    Text(
+      value,
+      style: TextStyle(
+          color: Color(0xFF6AD6F9),
+          fontSize: 15,
+          fontFamily: "Sora",
+          fontWeight: FontWeight.bold),
+    ),
+  ];
 }
