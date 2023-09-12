730db023e04a33e8f85d386575fefedcc075b763
justinrampengan <105626099+OrangJenius@users.noreply.github.com>
2023-09-12 15:02:49 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/OrangJenius/coldstorage""
    This reverts commit ad9f0befac68ce5a8fbe02be8eaf5ea244c24d4f.

---
+++ driver_cold_storage/lib/screens/forgot_pass3.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class forgot_Pass3 extends StatefulWidget {
+  String? generatedOtp;
+  @override
+  _forgot_PassState3 createState() => _forgot_PassState3();
+}
+
+class _forgot_PassState3 extends State<forgot_Pass3> {
+  bool _obscureNewPassword = true;
+  bool _obscureConfirmPassword = true;
+
+  void _toggleNewPasswordVisibility() {
+    setState(() {
+      _obscureNewPassword = !_obscureNewPassword;
+    });
+  }
+
+  void _toggleConfirmPasswordVisibility() {
+    setState(() {
+      _obscureConfirmPassword = !_obscureConfirmPassword;
+    });
+  }
+
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 32, top: 80),
+                child: Text(
+                  "Forgot\nPassword",
+                  style: TextStyle(
+                      fontFamily: 'Sora',
+                      fontSize: 36,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF6AD6F9)),
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.only(left: 32),
+                child: Text(
+                  "New Password",
+                  style: TextStyle(
+                      fontFamily: 'Sora',
+                      fontSize: 20,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF6AD6F9)),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 32, right: 32, top: 8),
+                child: TextFormField(
+                  controller: _newPasswordController,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(
+                        color: Color(0xff6AD6F9),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(
+                        color: Color(0xff6AD6F9),
+                      ),
+                    ),
+                    suffixIcon: GestureDetector(
+                      onTap: _toggleNewPasswordVisibility,
+                      child: Icon(
+                        _obscureNewPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                    ),
+                    suffixIconColor: Color(0xff6AD6F9),
+                    prefixIcon: Icon(Icons.lock),
+                    prefixIconColor: Color(0xff6AD6F9),
+                  ),
+                  obscureText: _obscureNewPassword,
+                ),
+              ),
+              SizedBox(height: 32),
+              Container(
+                padding: EdgeInsets.only(left: 32),
+                child: Text(
+                  "Confirm New Password",
+                  style: TextStyle(
+                      fontFamily: 'Sora',
+                      fontSize: 20,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF6AD6F9)),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 32, right: 32, top: 8),
+                child: TextFormField(
+                  controller: _confirmPasswordController,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(
+                        color: Color(0xff6AD6F9),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                      borderSide: BorderSide(
+                        color: Color(0xff6AD6F9),
+                      ),
+                    ),
+                    suffixIcon: GestureDetector(
+                      onTap: _toggleConfirmPasswordVisibility,
+                      child: Icon(
+                        _obscureConfirmPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                    ),
+                    suffixIconColor: Color(0xff6AD6F9),
+                    prefixIcon: Icon(Icons.lock),
+                    prefixIconColor: Color(0xff6AD6F9),
+                  ),
+                  obscureText: _obscureConfirmPassword,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 50), // Padding horizontal 32 dan vertikal 24
+                child: Container(
+                  width: double
+                      .infinity, // Ini akan membuat Container mengambil lebar maksimal yang tersedia.
+                  height:
+                      50, // Anda bisa mengatur tinggi tombol sesuai kebutuhan Anda.
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              30), // border radius sebesar 30
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(5),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF6AD6F9)),
+                    ),
+                    onPressed: () {
+                      if (_newPasswordController.text ==
+                          _confirmPasswordController.text) {
+                        print(_newPasswordController);
+                        print(_confirmPasswordController);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Login()),
+                        );
+                      }
+                    },
+                    child: Text('Change Password',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Sora",
+                        )), // Anda juga bisa mengatur style dari teks jika diperlukan.
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ driver_cold_storage/lib/screens/report.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'reportDetail.dart';
+
+class reportScreen extends StatefulWidget {
+  @override
+  _reportScreenState createState() => _reportScreenState();
+}
+
+class _reportScreenState extends State<reportScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 16, top: 24, bottom: 16),
+                child: Text(
+                  "Report Services",
+                  style: TextStyle(
+                    color: Color(0xFF6AD6F9),
+                    fontFamily: 'Sora',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Divider(
+                  height: 1, // Atur tinggi garis sesuai kebutuhan
+                  color: Color(0xFFd9d9d9), // Atur warna garis sesuai kebutuhan
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: InkWell(
+                  onTap: () => {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => reportDetailScreen()))
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        style: BorderStyle.solid,
+                        color: Color(0xffD7D7D7),
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xffffffff),
+                          spreadRadius: 1,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'ID: ',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: 'Sora',
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'BJE290012KLOP',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontFamily: 'Sora',
+                                  color: Color(0xFF6AD6F9),
+                                  fontWeight: FontWeight.w400,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          "7 Juli 2023 | 20.00",
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 12),
+                        ),
+                        Text(
+                          "Note: andkjnqwujdiuna",
+                          style: TextStyle(
+                              fontFamily: 'Sora',
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 10,
+                              color: Color(0xffA5A5A5)),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "Qty",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 12),
+                                ),
+                                Text(
+                                  "10",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 13),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "Weights",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 12),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: '10',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Sora',
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: 'Kg',
+                                        style: TextStyle(
+                                          fontSize: 10,
+                                          fontFamily: 'Sora',
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "price",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 12),
+                                ),
+                                Text(
+                                  "Rp 100.000",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 13),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "Amount Left",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 12),
+                                ),
+                                Text(
+                                  "Rp 99.000",
+                                  style: TextStyle(
+                                      fontFamily: 'Sora',
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 13),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
